8021d30b6726738bbad3d0ded5740e67e206f27e
RenataBalbino <renata.b.silva2002@gmail.com>
2023-10-18 11:35:03 -0300

    a

--- serve/src/main/java/server/Main.java
+++ serve/src/main/java/server/Main.java
@@ -10,13 +10,11 @@ public class Main {
             
             // cria o objeto que vai configurar o server
             Configuration config = new Configuration();
-            config.setHostname("localhost");        // nome do server a ser criado
-            config.setPort(5002);                       // define a porta que o server vai ser acessado
+            config.setHostname("localhost");                                                            // nome do server a ser criado
+            config.setPort(3001);                                                                           // define a porta que o server vai ser acessado
 
             // cria um server do socket.io
             SocketIOServer server = new SocketIOServer(config);             
-            SocketConfig sockConfig = new SocketConfig();
-            sockConfig.setReuseAddress(true);  
 
             server.addConnectListener(client -> {
                 System.out.println("Client Connected");
@@ -27,15 +25,20 @@ public class Main {
             server.addEventListener("message", String.class, new DataListener<String>() {
 
                 @Override
-                public void onData(SocketIOClient cliente, String data, AckRequest ackRequest) throws Exception {
+                public void onData(SocketIOClient client, String data, AckRequest ackRequest) throws Exception {
                     System.out.println("Received message from client: " + data);
+                    ackRequest.sendAckData("Mensagem recebida");
                 }            
 
             });
 
+            server.addDisconnectListener(client -> {
+                System.out.println("Client Disconnected");
+            });
+
             server.start();
             System.out.println("Servidor funcionando em http://" + config.getHostname() + ":" + config.getPort());
-            
+
         } catch (Exception e) {
             System.err.println("An error occurred: " + e.getMessage());
             System.exit(1);
