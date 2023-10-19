2321c2d900a13b5d48b2bac829ce65e468eeecdd
RenataBalbino <renata.b.silva2002@gmail.com>
2023-10-19 12:18:44 -0300

    front

--- serve/src/main/java/server/Main.java
+++ serve/src/main/java/server/Main.java
@@ -6,45 +6,14 @@ import com.corundumstudio.socketio.listener.DataListener;
 public class Main {
     public static void main(String[] args) {
 
-        try {
-            
-            // cria o objeto que vai configurar o server
-            Configuration config = new Configuration();
-            config.setHostname("localhost");                                                            // nome do server a ser criado
-            config.setPort(3001);                                                                           // define a porta que o server vai ser acessado
+        Configuration config = new Configuration();
+        config.setHostname("localhost");
+        config.setPort(3001);
 
-            // cria um server do socket.io
-            SocketIOServer server = new SocketIOServer(config);             
-
-            server.addConnectListener(client -> {
-                System.out.println("Client Connected");
-            });
+        SocketIOServer server = new SocketIOServer(config);
         
-
-            // escutando o server e realizando uma ação caso algum evento ocorra
-            server.addEventListener("message", String.class, new DataListener<String>() {
-
-                @Override
-                public void onData(SocketIOClient client, String data, AckRequest ackRequest) throws Exception {
-                    System.out.println("Received message from client: " + data);
-                    ackRequest.sendAckData("Mensagem recebida");
-                }            
-
-            });
-
-            server.addDisconnectListener(client -> {
-                System.out.println("Client Disconnected");
-            });
-
-            server.start();
-            System.out.println("Servidor funcionando em http://" + config.getHostname() + ":" + config.getPort());
-
-        } catch (Exception e) {
-            System.err.println("An error occurred: " + e.getMessage());
-            System.exit(1);
-        }
-        
-        
+        server.start();
+        System.out.println("Server inicializado em: http://" + config.get;
 
     }
 }
